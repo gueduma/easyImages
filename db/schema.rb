bd1d5dedbbdff065474024e7e484c04dcb4a44db
Guilherme Amaral <guiedu.martins@hotmail.com>
2017-06-24 22:26:20 +0000

    adicionado funcionalidade de login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524050805) do
+ActiveRecord::Schema.define(version: 20170624030039) do
 
   create_table "galeria", force: :cascade do |t|
     t.string   "nome"
@@ -30,6 +30,22 @@ ActiveRecord::Schema.define(version: 20170524050805) do
     t.datetime "updated_at",                 null: false
     t.string   "galerias"
     t.bigint   "id_galeria"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "usuarios", force: :cascade do |t|
