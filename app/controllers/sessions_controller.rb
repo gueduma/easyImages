619b154a8d616e1ee51cf78c3564ec387e0ff14b
Guilherme Amaral <guiedu.martins@hotmail.com>
2017-06-25 16:27:20 +0000

    alterações no gemfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
+      if user.activated? == false
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
